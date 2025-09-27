e66275e444f40fce2c1681c012aa1b698319cf03
x-vmaier <valentin16.maier@gmail.com>
2025-09-27 23:02:28 +0200

    Implement derivative for MSE

--- examples/simple_model.cpp
+++ examples/simple_model.cpp
@@ -2,12 +2,13 @@
 #include "polann/layers/dense.hpp"
 #include "polann/core/model_builder.hpp"
 #include "polann/utils/activation_functions.hpp"
-#include "polann/utils/loss_functions.hpp"
+#include "polann/loss/mse.hpp"
 #include "polann/utils/io.hpp"
 
 using namespace polann::core;
 using namespace polann::layers;
 using namespace polann::utils;
+using namespace polann::loss;
 
 int main()
 {
@@ -25,6 +26,10 @@ int main()
     // Make prediction
     std::array<float, 1> outputs = model.predict(inputs);
     std::cout << "Output: " << outputs << std::endl;
+
+    // Calculate loss
+    std::array<float, 1> label = {1.0f};
+    std::cout << "Loss: " << MSE::compute(outputs, label) << std::endl;
 
     return 0;
 }
